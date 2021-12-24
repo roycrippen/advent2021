2433a195163d1ba54ede04c1da83f606bbd6a2bb
roy <roy.crippen@archarithms.com>
2021-12-24 10:19:29 -0500

    day07

--- crates/day07/src/main.rs
+++ crates/day07/src/main.rs
@@ -1,40 +1,78 @@
 use utils::InputType;
+use std::collections::HashMap;
 
 fn main() {
-    let aaa = read_input(InputType::Input);
-    println!("Day06 part a = {}", part_a(aaa));
-    println!("Day06 part b = {}", part_b(aaa));
+    let xs = read_input(InputType::Input);
+    println!("Day06 part a = {}", part_a(&xs)); // 348664
+    println!("Day06 part b = {}", part_b(&xs));
 }
 
-fn part_a(_xs: ()) -> usize {
-    todo!()
+fn part_a(xs: &Vec<usize>) -> usize {
+    let max = *xs.iter().max().unwrap() as usize;
+    let min = *xs.iter().min().unwrap() as usize;
+    let ys: Vec<usize> = (min..=max).map(|v| distance_a(&v, xs)).collect();
+    let res = *ys.iter().min().unwrap();
+
+    // let zipped: Vec<(&usize, usize)> = xs.iter().zip(ys).collect();
+    // println!("{:?}", zipped);
+    res
 }
 
-fn part_b(_xs: ()) -> usize {
-    todo!()
+fn part_b(xs: &Vec<usize>) -> usize {
+    let max = *xs.iter().max().unwrap() as usize;
+    let min = *xs.iter().min().unwrap() as usize;
+    let delta = max - min;
+    let cache: HashMap<usize, usize> = (0..=delta).map(|v| (v, (0..=v).sum())).collect();
+    let ys: Vec<usize> = (min..=max).map(|v| distance_b(&v, xs, &cache)).collect();
+    let res = *ys.iter().min().unwrap();
+
+    // let zipped: Vec<(&usize, usize)> = xs.iter().zip(ys).collect();
+    // println!("{:?}", zipped);
+    res
 }
 
-fn read_input(input_type: InputType) -> () {
+fn distance_a(destination: &usize, xs: &Vec<usize>) -> usize {
+    xs.iter().fold(0, |acc, x| if x <= destination { acc + destination - x } else { acc + x - destination })
+}
+
+fn distance_b(destination: &usize, xs: &Vec<usize>, cache: &HashMap<usize, usize>) -> usize {
+    xs.iter().fold(0, |acc, x|
+        if x <= destination {
+            let delta = destination - x;
+            acc + cache.get(&delta).unwrap()
+        } else {
+            let delta = x - destination;
+            acc + cache.get(&delta).unwrap()
+        })
+}
+
+fn read_input(input_type: InputType) -> Vec<usize> {
     let data = {
         match input_type {
             InputType::Sample => include_str!("sample.txt"),
             InputType::Input => include_str!("input.txt"),
         }
     };
-    println!("Source: {:?}, length: {}", input_type, data.len());
 
-    ()
+    data.split(",")
+        .map(|x| x.parse().unwrap())
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{read_input, part_a, part_b};
+    use utils::InputType;
+
     #[test]
     fn test_part_a() {
-        assert_eq!(true, true);
+        let xs = read_input(InputType::Sample);
+        assert_eq!(37, part_a(&xs));
     }
 
     #[test]
     fn test_part_b() {
-        assert_eq!(true, true);
+        let xs = read_input(InputType::Sample);
+        assert_eq!(168, part_b(&xs));
     }
 }
